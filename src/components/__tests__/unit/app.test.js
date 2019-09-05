68998e5c10b6dabdd1c24638a91e52cc327c6540
Fernando Nieto <lestairon@MacBook-Pro-de-Fernando.local>
2019-09-05 09:12:17 -0500

    Add jest mock for offline test.

--- src/components/__tests__/unit/app.test.js
+++ src/components/__tests__/unit/app.test.js
@@ -2,6 +2,22 @@ import App from "../../App";
 import React from "react";
 import { render } from "@testing-library/react";
 import "jest-styled-components";
+import { firestoreDB } from "../../../config/config";
+
+jest.spyOn(firestoreDB, "collection").mockImplementation(() => ({
+  onSnapshot: cb => {
+    setTimeout(() =>
+      cb({
+        docChanges: () => [
+          {
+            type: "added",
+            doc: { data: () => ({ name: "test", score: 100 }) }
+          }
+        ]
+      })
+    );
+  }
+}));
 
 test("renders correctly the Main component", () => {
   const { container } = render(<App />);
